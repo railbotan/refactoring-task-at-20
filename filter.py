399107cc00a7b654d39e6b16970e04e2ca5ee77d
Andrew <70693527+vazik66@users.noreply.github.com>
2021-11-15 15:15:15 +0500

    Stage 3 & 4.
    1. Replaced cycles with matrix transformations
    2. Now usable from terminal

--- filter.py
+++ filter.py
@@ -2,38 +2,36 @@ from PIL import Image
 import numpy as np
 
 
-def main():
-    img = Image.open("img2.jpg")
+def get_avg_brightness(img_data, mosaic_size, x, y):
+    return np.average(img_data[x : x + mosaic_size, y : y + mosaic_size])
 
-    img_data = np.array(img)
-    img_size = img_data.shape
-    mosaic_size = 10
-    grayscale_step = 50
 
+def save_image(data, file_name, path):
+    Image.fromarray(data).save(f'{file_name}.{path.split(".")[-1]}')
+
+
+def create_mosaic(img_data, img_size, mosaic_size, grayscale_step):
     for x in range(0, img_size[0], mosaic_size):
         for y in range(0, img_size[1], mosaic_size):
             avg_brightness = get_avg_brightness(img_data, mosaic_size, x, y)
-            apply_grayscale(img_data, mosaic_size, grayscale_step, x, y, avg_brightness)
+            img_data[x : x + mosaic_size, y : y + mosaic_size] = avg_brightness - avg_brightness % grayscale_step
+            
 
-    result = Image.fromarray(img_data)
-    result.save("res.jpg")
+def main():
+    path = input("Enter image path: ")
+    output_name = input("Enter name of output file: ")
+    mosaic_size = int(input("Enter mosaic size: "))
+    grayscale_step = int(input("Enter grayscale step: "))
 
+    img_data = np.array(Image.open(path))
+    img_size = img_data.shape
 
-def apply_grayscale(img_data, mosaic_size, grayscale_step, x, y, avg_brightness):
-    for x1 in range(x, x + mosaic_size):
-        for y1 in range(y, y + mosaic_size):
-            img_data[x1][y1][0] = img_data[x1][y1][1] = img_data[x1][y1][2] = (
-                int(avg_brightness // grayscale_step) * grayscale_step
-            )
-
-
-def get_avg_brightness(img_data, mosaic_size, x, y):
-    avg_brightness = 0
-    for x1 in range(x, x + mosaic_size):
-        for y1 in range(y, y + mosaic_size):
-            avg_brightness += int(sum(img_data[x1][y1])) // 3
-    return int(avg_brightness // mosaic_size ** 2)
+    create_mosaic(img_data, img_size, mosaic_size, grayscale_step)
+    save_image(img_data, output_name, path)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        quit()
