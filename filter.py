c654aaed1c4046849ddb3ef83f33491e9b7ec630
Ivan-Dyncan <dodzegun@bk.ru>
2021-11-17 20:57:47 +0500

    1 этап

--- filter.py
+++ filter.py
@@ -5,23 +5,23 @@ arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i <= a - 1:
     j = 0
-    while j < a1 - 11:
+    while j <= a1 - 1:
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+                r = arr[n][n1][0]
+                g = arr[n][n1][1]
+                b = arr[n][n1][2]
+                M = int(r) + int(g) + int(b)
+                s += M/3
         s = int(s // 100)
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+                arr[n][n1][0] = np.uint8((s // 50) * 50)
+                arr[n][n1][1] = np.uint8((s // 50) * 50)
+                arr[n][n1][2] = np.uint8((s // 50) * 50)
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
