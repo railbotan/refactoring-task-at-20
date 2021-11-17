5de0f032576175909d5f6ae811b860a873d9a4c5
Ivan-Dyncan <dodzegun@bk.ru>
2021-11-17 23:38:32 +0500

    этап 3

--- filter.py
+++ filter.py
@@ -2,34 +2,25 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
-size = 2
+size = 10
 gradation = 50
 a = len(arr)
 a1 = len(arr[1])
 
 
 def findMidValue(i, j, size):
-    s=0
     global arr
-    for n in range(i, i + size):
-        for n1 in range(j, j + size):
-            r = arr[n][n1][0]
-            g = arr[n][n1][1]
-            b = arr[n][n1][2]
-            M = int(r) + int(g) + int(b)
-            s += M / 3
-    s = int(s // size**2)
+    matrix = arr[i:i + size, j:j + size, ::]
+    b = list(matrix.reshape(1, matrix.shape[0] * matrix.shape[1] * 3))[0]
+    b = list(map(lambda x: int(x)/3, b))
+    s = int(sum(b)// size**2)
     return s
 
 
 def changeColour(i, j, size, gradation):
     global arr
     s = findMidValue(i, j, size)
-    for n in range(i, i + size):
-        for n1 in range(j, j + size):
-            arr[n][n1][0] = np.uint8((s // gradation) * gradation)
-            arr[n][n1][1] = np.uint8((s // gradation) * gradation)
-            arr[n][n1][2] = np.uint8((s // gradation) * gradation)
+    arr[i:i + size, j:j + size, ::] = s
 
 
 i = 0
