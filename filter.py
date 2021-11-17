3edef3c76433b7f1150188dbe01dec2badedf8c7
Ivan-Dyncan <dodzegun@bk.ru>
2021-11-17 21:10:47 +0500

    2 этап

--- filter.py
+++ filter.py
@@ -2,26 +2,41 @@ from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
 arr = np.array(img)
+size = 10
+gradation = 50
 a = len(arr)
 a1 = len(arr[1])
+
+
+def findMidValue(i, j, size):
+    s=0
+    global arr
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            r = arr[n][n1][0]
+            g = arr[n][n1][1]
+            b = arr[n][n1][2]
+            M = int(r) + int(g) + int(b)
+            s += M / 3
+    s = int(s // 100)
+    return s
+
+
+def changeColour(i, j, size, gradation):
+    global arr
+    s = findMidValue(i, j, size)
+    for n in range(i, i + size):
+        for n1 in range(j, j + size):
+            arr[n][n1][0] = np.uint8((s // gradation) * gradation)
+            arr[n][n1][1] = np.uint8((s // gradation) * gradation)
+            arr[n][n1][2] = np.uint8((s // gradation) * gradation)
+
+
 i = 0
 while i <= a - 1:
     j = 0
     while j <= a1 - 1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = arr[n][n1][0]
-                g = arr[n][n1][1]
-                b = arr[n][n1][2]
-                M = int(r) + int(g) + int(b)
-                s += M/3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = np.uint8((s // 50) * 50)
-                arr[n][n1][1] = np.uint8((s // 50) * 50)
-                arr[n][n1][2] = np.uint8((s // 50) * 50)
+        changeColour(i, j, size, gradation)
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
