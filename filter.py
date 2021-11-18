d48cfec75176817b155dc7669e46f5256e1fe919
nicosare <dgalkin7@gmail.com>
2021-11-19 02:54:13 +0500

    Код соответствует PEP8
    Были переименованы переменные, названия стали более понятны
    
    Добавлена возможность управлять размерами мозайки и градациями серого
    
    Были выделены функции

--- filter.py
+++ filter.py
@@ -1,28 +1,45 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 9:
-    j = 0
-    while j < a1 - 9:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n0 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n0 // 3 + n2 // 3 + n3 // 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def do_filter(count_gradations, mosaic_size):
+    img = Image.open("img2.jpg")
+    arr = np.array(img)
+    width = len(arr)
+    height = len(arr[1])
+    gradations = 255 // count_gradations
+    pixel_x = 0
+
+    def find_average_brightness(array, px_width, px_height, mosaic):
+        sum_brightness = 0
+        for cell_pixel_x in range(px_width, px_width + mosaic):
+            for cell_pixel_y in range(px_height, px_height + mosaic):
+                px_coord = array[cell_pixel_x][cell_pixel_y]
+                r = px_coord[0]
+                g = px_coord[1]
+                b = px_coord[2]
+                median = r // 3 + g // 3 + b // 3
+                sum_brightness += median
+        average_bright = int(sum_brightness // mosaic_size ** 2)
+        return average_bright
+
+    def paint_cell(array, px_width, px_height, mosaic):
+        for cell_pixel_x in range(px_width, px_width + mosaic):
+            for cell_pixel_y in range(px_height, px_height + mosaic):
+                px_coord = array[cell_pixel_x][cell_pixel_y]
+                px_coord[0] = int(average_brightness // gradations) * gradations
+                px_coord[1] = int(average_brightness // gradations) * gradations
+                px_coord[2] = int(average_brightness // gradations) * gradations
+
+    while pixel_x < width - mosaic_size + 1:
+        pixel_y = 0
+        while pixel_y < height - mosaic_size + 1:
+            average_brightness = find_average_brightness(arr, pixel_x, pixel_y, mosaic_size)
+            paint_cell(arr, pixel_x, pixel_y, mosaic_size)
+            pixel_y = pixel_y + mosaic_size
+        pixel_x = pixel_x + mosaic_size
+    res = Image.fromarray(arr)
+    res.save('res.jpg')
+
+
+do_filter(10, 52)
