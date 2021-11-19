657033a36337e9453324e2241cb1989e09eb19ab
Wanderer76 <ateplinsky@mail.ru>
2021-11-19 19:29:04 +0500

    сделал 2 этап

--- filter.py
+++ filter.py
@@ -1,33 +1,49 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
 
-i = 0
-while i < a + 10:
-    j = 0
-    while j < a1 + 10:
-        s = 0
-        for n in range(i, i + 10):
-            for col in range(j, j + 10):
-                n1 = arr[n][col][0]
-                n2 = arr[n][col][1]
-                n3 = arr[n][col][2]
-                M = int(n1) + int(n2) + int(n3)
-                s += M
-        s = int(s // 100)
+def pixel_colors_sum(elements):
+    n1 = elements[0]
+    n2 = elements[1]
+    n3 = elements[2]
+    return int(n1) + int(n2) + int(n3)
 
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                greyValue = int(s // 50) * 50 // 3
-                arr[n][n1][0] = int(s // 50) * 50 // 3
-                arr[n][n1][1] = int(s // 50) * 50 // 3
-                arr[n][n1][2] = int(s // 50) * 50 // 3
-        j = j + 10
-    i = i + 10
 
-res = Image.fromarray(arr)
+def calculate_average_value(arr, y, x):
+    result = 0
+    count = blockSize * blockSize
+    for row in range(y, y + blockSize):
+        for col in range(x, x + blockSize):
+            result += pixel_colors_sum(arr[row][col])
+    return result // count
+
+
+def set_grey_color(arr, averageValue, blockSize, i, j, step):
+    for row in range(i, i + blockSize):
+        for col in range(j, j + blockSize):
+            greyValue = averageValue // 50 * step // 3
+            arr[row][col][0] = greyValue
+            arr[row][col][1] = greyValue
+            arr[row][col][2] = greyValue
+
+
+def transform_image(file_name, gradation, blockSize):
+    image = Image.open(file_name)
+    arr = np.array(image)
+    height = len(arr)
+    width = len(arr[1])
+    step = 255 // gradation
+
+    for i in range(0, height, blockSize):
+        for j in range(0, width, blockSize):
+            averageValue = calculate_average_value(arr, i, j)
+            set_grey_color(arr, averageValue, blockSize, i, j, step)
+
+    return Image.fromarray(arr)
+
+
+gradation = 5
+blockSize = 10
+
+res = transform_image("img2.jpg", gradation, blockSize)
 res.save('res.jpg')
