f5ddead5ab7d13d0f41bed1b056f00c7bf7fd317
BobylevTimofey <bobylevvg@gmail.com>
2021-11-19 18:43:04 +0500

    Заменил по возможности циклы матричными преобразованиями, дополнительно перименовал некоторые переменные

--- filter.py
+++ filter.py
@@ -4,40 +4,26 @@ import numpy as np
 
 def apply_filter(mosaic_size, count_gradations):
     img = Image.open("img2.jpg")
-    arr = np.array(img)
-    len_width = len(arr)
-    len_height = len(arr[1])
+    data_img = np.array(img)
+    size_img = data_img.shape
 
-    px_width = 0
-    while px_width < len_width - mosaic_size - 1:
-        px_height = 0
-        while px_height < len_height - mosaic_size - 1:
-            average_brightness = find_average_brightness_cell(arr, px_width, px_height, mosaic_size)
-            paint_cell(arr, px_width, px_height, mosaic_size, average_brightness, count_gradations)
-            px_height = px_height + mosaic_size
-        px_width = px_width + mosaic_size
-    res = Image.fromarray(arr)
+    for x in range(0, size_img[0], mosaic_size):
+        for y in range(0, size_img[1], mosaic_size):
+            paint_cell(data_img, x, y, mosaic_size, count_gradations)
+
+    res = Image.fromarray(data_img)
     res.save('res.jpg')
 
 
 def find_average_brightness_cell(arr, px_width, px_height, mosaic_size):
-    sum_brightness = 0
-    for i in range(px_width, px_width + mosaic_size):
-        for j in range(px_height, px_height + mosaic_size):
-            r = arr[i][j][0]
-            g = arr[i][j][1]
-            b = arr[i][j][2]
-            median = r // 3 + g // 3 + b // 3
-            sum_brightness += median
-    average_brightness = sum_brightness // mosaic_size ** 2
-    return average_brightness
+    return np.average(arr[px_width:px_width + mosaic_size, px_height: px_height + mosaic_size])
 
 
-def paint_cell(arr, px_width, px_height, mosaic_size, average_brightness, count_gradations):
-    for i in range(px_width, px_width + mosaic_size):
-        for j in range(px_height, px_height + mosaic_size):
-            gradations = 255 // count_gradations
-            arr[i][j][0] = average_brightness // gradations * gradations
-            arr[i][j][1] = average_brightness // gradations * gradations
-            arr[i][j][2] = average_brightness // gradations * gradations
+def paint_cell(arr, px_width, px_height, mosaic_size, count_gradations):
+    average_brightness = find_average_brightness_cell(arr, px_width, px_height, mosaic_size)
+    gradations = 255 // count_gradations
+    px_brightness = average_brightness // gradations * gradations
+    arr[px_width:px_width + mosaic_size, px_height:px_height + mosaic_size, ][:] = px_brightness
 
+
+apply_filter(10, 4)
