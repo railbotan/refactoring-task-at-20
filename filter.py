b98c22339f6994d4466eb23c5450c00e7fe3cbf7
nicosare <dgalkin7@gmail.com>
2021-11-19 21:14:09 +0500

    переписал код в консольную утилиту, которой на вход подаются имя результата, путь изображения, количество градаций и размер мозайки

--- filter.py
+++ filter.py
@@ -2,27 +2,32 @@ from PIL import Image
 import numpy as np
 
 
-def do_filter(count_gradations, mosaic_size):
-    img = Image.open("img2.jpg")
-    array = np.array(img)
-    size_img = array.shape
+def do_filter():
+    path_img = input("Введите путь к изображению: ")
+    name = input("Введите имя выходного изображения: ")
+    count_gradations = int(input("Введите количество градаций: "))
+    mosaic_size = int(input("Введите размер мозайки: "))
+    img = Image.open(path_img)
+    img_data = np.array(img)
+    size_img = img_data.shape
 
     for i in range(0, size_img[0], mosaic_size):
         for j in range(0, size_img[1], mosaic_size):
-            paint_cell(array, i, j, mosaic_size, count_gradations)
+            paint_cell(img_data, i, j, mosaic_size, count_gradations)
 
-    res = Image.fromarray(array)
-    res.save('res.jpg')
-
-
-def find_average_brightness(arr, px_width, px_height, mosaic):
-    return np.average(arr[px_width: px_width + mosaic, px_height: px_height + mosaic])
+    res = Image.fromarray(img_data)
+    path_output = path_img[0:path_img.rindex('\\') + 1]
+    res.save(f"{path_output}{name}")
 
 
-def paint_cell(arr, width, height, mosaic_size, count_gradations):
+def find_average_brightness(img_data, width, height, mosaic_size):
+    return np.average(img_data[width: width + mosaic_size, height: height + mosaic_size])
+
+
+def paint_cell(img_data, width, height, mosaic_size, count_gradations):
     gradations = 255 // count_gradations
-    average_brightness = find_average_brightness(arr, width, height, mosaic_size)
-    arr[width: width + mosaic_size, height: height + mosaic_size] = average_brightness // gradations * gradations
+    average_brightness = find_average_brightness(img_data, width, height, mosaic_size)
+    img_data[width: width + mosaic_size, height: height + mosaic_size] = average_brightness // gradations * gradations
 
 
-do_filter(100, 20)
+do_filter()
