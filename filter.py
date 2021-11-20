95a53e98da63e9ac19766fde965058c79033a8fa
shiriaeva <Elena.Shiriaeva@urfu.me>
2021-11-20 21:21:51 +0500

    этап 4
    добавлено взаимодействие с консолью

--- filter.py
+++ filter.py
@@ -7,23 +7,28 @@ def get_mid_bright(block, element_side, step):
     return int(midBright // step) * step / 3
 
 
-def transform_img(image_file, element_side, gradation):
-    img = Image.open(image_file)
-    arr = np.array(img)
+def transform_img(arr, element_side, step):
     length = len(arr)
     width = len(arr[1])
-    step = 255 // (gradation - 1)
-
     for row in range(0, length, element_side):
         for col in range(0, width, element_side):
             arr[row: row + element_side, col: col + element_side] = get_mid_bright(
                 arr[row: row + element_side, col: col + element_side], element_side, step
             )
-    res = Image.fromarray(arr)
-    res.save('res.jpg')
 
-    return res
+    return arr
 
 
-transform_img('img2.jpg', 2, 5)
+def main():
+    img = Image.open(input("Введите имя конвертируемого файла: "))
+    arr = np.array(img)
+    element_side = int(input("Введите размер элемента мозаики: "))
+    gradation = int(input("Введите количество градаций серого: "))
+    step = 255 // (gradation - 1)
 
+    res = Image.fromarray(transform_img(arr, element_side, step))
+    res.save(input("Введите имя файла для сохранения результата: "))
+
+
+if __name__ == '__main__':
+    main()
