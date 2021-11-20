7ad00d6328680b1ab463851bf4f323778e83fa17
shiriaeva <Elena.Shiriaeva@urfu.me>
2021-11-20 21:04:14 +0500

    3 этап
    частичное избавление от ручных циклов

--- filter.py
+++ filter.py
@@ -2,26 +2,9 @@ from PIL import Image
 import numpy as np
 
 
-def mid_bright(arr, x, y, element_side):
-    s = 0
-    for row in range(x, x + element_side):
-        for col in range(y, y + element_side):
-            r = arr[row][col][0]
-            g = arr[row][col][1]
-            b = arr[row][col][2]
-            s += int(r) + int(g) + int(b)
-
-    midBright = int(s // (element_side * element_side))
-    return midBright
-
-
-def set_grey(arr, midBright, x, y, element_side, step):
-    for row in range(x, x + element_side):
-        for col in range(y, y + element_side):
-            shade_of_grey = int(midBright // step) * step / 3
-            arr[row][col][0] = shade_of_grey
-            arr[row][col][1] = shade_of_grey
-            arr[row][col][2] = shade_of_grey
+def get_mid_bright(block, element_side, step):
+    midBright = block[:element_side, :element_side].sum() // (element_side * element_side)
+    return int(midBright // step) * step / 3
 
 
 def transform_img(image_file, element_side, gradation):
@@ -33,9 +16,9 @@ def transform_img(image_file, element_side, gradation):
 
     for row in range(0, length, element_side):
         for col in range(0, width, element_side):
-            midBright = mid_bright(arr, row, col, element_side)
-            set_grey(arr, midBright, row, col, element_side, step)
-
+            arr[row: row + element_side, col: col + element_side] = get_mid_bright(
+                arr[row: row + element_side, col: col + element_side], element_side, step
+            )
     res = Image.fromarray(arr)
     res.save('res.jpg')
 
